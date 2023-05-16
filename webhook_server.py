e46e244a1f39ea212e58251d65c4d333e1bcfeb3
brian <btchan@telus.net>
2023-05-16 16:37:45 -0600

    Updated deprecated commands.

--- webhook_server.py
+++ webhook_server.py
@@ -8,7 +8,7 @@ def handle_webhook():
     return 'Webhook received'
 
 if __name__ == '__main__':
-    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
     context.load_cert_chain('/home/ubuntu/ssl_info/certificate.crt', '/home/ubuntu/ssl_info/private.key')
 
     app.run(host='0.0.0.0', port=443, ssl_context=context, threaded=True)
