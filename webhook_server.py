4a90c5db6b51d6eb14e0da0e7919569439edcc46
brian <btchan@telus.net>
2023-05-16 21:32:09 -0600

    having handle_webhook return a valid response

--- webhook_server.py
+++ webhook_server.py
@@ -10,6 +10,8 @@ def handle_webhook():
     if request.method == 'POST':
         subprocess.call(['/home/ubuntu/chess_data/CI-CD-for-chess-data-pipeline/update_repo.sh'])
 
+    return 'Webhook received'
+
 if __name__ == '__main__':
     context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
     context.load_cert_chain('/home/ubuntu/ssl_info/certificate.crt', '/home/ubuntu/ssl_info/private.key')
